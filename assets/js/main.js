eead1bb2f473451178140ccd5a07ed2eda8f788b
rabby-shek <rabbyshek1998@gmail.com>
2024-06-12 19:15:25 +0600

    design complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,15 +26,34 @@ navLinks.forEach(link => link.addEventListener('click', () => {
 /*=============== ADD BLUR HEADER ===============*/
 const scrollHeader = () => {
     const header = document.getElementById('header');
-    this.scrollY>=50 ? header.classList.add('blur-header') : header.classList.remove('blur-header');
+    this.scrollY >= 50 ? header.classList.add('blur-header') : header.classList.remove('blur-header');
 }
 window.addEventListener('scroll', scrollHeader);
 
 /*=============== SHOW SCROLL UP ===============*/
-
+const scrollUp = () => {
+    const scrollup = document.getElementById('scrollUp');
+    this.scrollY >= 350 ? scrollup.classList.add('show-scroll') : scrollup.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
+const sections = document.querySelectorAll('section[id]');
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight,
+         sectionTop = current.offsetTop - 58,
+        sectionId = current.getAttribute('id'),
+        sectionClass = document.querySelector('.nav-menu a[href*=' + sectionId+ ']');
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionClass.classList.add('active-link')
+        } else {
+            sectionClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
