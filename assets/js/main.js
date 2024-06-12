a8c8f353155b181326c8aada5fa6eb86b8d50b0f
rabby-shek <rabbyshek1998@gmail.com>
2024-06-12 13:59:06 +0600

    complete: : mobile menu navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,32 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('navMenu'),
+    navToggle = document.getElementById('navToggle'),
+    navClose = document.getElementById('navClose');
 
+// Show Menu
+
+if (navMenu) {
+    navToggle.addEventListener('click', () => {
+        console.log("clicked");
+        navMenu.classList.add('show-menu');
+    });
+}
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
-
+let navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach(link => link.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+}));
 
 /*=============== ADD BLUR HEADER ===============*/
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
